8a516d81fd698960627fbcbe94b53d90eded8f9e
Dominic.W.Kantrude <dominic.kantrude52@yahoo.com>
2019-04-22 13:57:15 -0500

    added facility options

--- src/Models/Plants/Seed.cs
+++ src/Models/Plants/Seed.cs
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-using System;
-using Trestlebridge.Interfaces;
-namespace Trestlebridge.Models.Plants
-{
-    public class Seed : IResource, ISeedProducing
-    {
-        private int _seedsProduced;
-        public string Type { get; }
-        //TODO   fix seed get
-        public double SeedProduced { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public double Harvest () {
-            return _seedsProduced;
-        }
-        public override string ToString () {
-            return $"Yum!";
-        }
-    }
-=======
 using System;
 using Trestlebridge.Interfaces;
 
@@ -39,5 +20,4 @@ namespace Trestlebridge.Models.Plants
             return $"Yum!";
         }
     }
->>>>>>> master
 }
