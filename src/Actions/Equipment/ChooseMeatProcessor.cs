c6f289a978d9dfac4585af3d01efaef8e293993d
Dominic.W.Kantrude <dominic.kantrude52@yahoo.com>
2019-04-26 10:23:05 -0500

    small changes

--- src/Actions/Equipment/ChooseMeatProcessor.cs
+++ src/Actions/Equipment/ChooseMeatProcessor.cs
@@ -5,22 +5,31 @@ using Trestlebridge.Interfaces;
 
 namespace Trestlebridge.Models.Equipment
 {
-  public class ChooseMeatProcessor
-  {
-
-    public static void CollectInput(Farm farm)
+    public class ChooseMeatProcessor
     {
-      var list = farm.CreateFacilitiesList();
-      for (int i = 0; i < list.Count; i++)
-      {
-          Console.WriteLine($"{i} ");
-          Console.WriteLine($" {list[i][0]}");
-      }
 
+        public static void CollectInput(Farm farm)
+        {
+            var list = farm.CreateFacilitiesList();
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i][0].GetType().Name);
+                if (list[i][0].GetType().Name != "GrazingField" && list[i][0].GetType().Name != "ChickenHouse")
+                {
+                    list.RemoveAt(i);
+                    i--;
+                    Console.WriteLine(i);
+                }
 
+                //return animal.Butcher();
 
-      //return animal.Butcher();
+            }
 
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine($"{i}  {list[i][0].GetType().Name}");
+
+            }
+        }
     }
-  }
 }
