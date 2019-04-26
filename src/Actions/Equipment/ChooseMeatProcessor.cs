bc4fd982db74874dc31d79c1367b21aa061d4bca
justinavickers <justinav38@gmail.com>
2019-04-26 11:56:13 -0500

    fixes display on duck house and chicken house

--- src/Actions/Equipment/ChooseMeatProcessor.cs
+++ src/Actions/Equipment/ChooseMeatProcessor.cs
@@ -1,36 +1,32 @@
 using System;
 using System.Collections.Generic;
 using Trestlebridge.Interfaces;
+using Trestlebridge.Models;
 
 
 namespace Trestlebridge.Models.Equipment
 {
-    public class ChooseMeatProcessor
+  public class ChooseMeatProcessor
+  {
+
+    public static void CollectInput(Farm farm)
     {
-
-        public static void CollectInput(Farm farm)
+      var list = farm.CreateFacilitiesList();
+      for (int i = 0; i < list.Count; i++)
+      {
+        if (list[i][0].GetType().Name != "GrazingField" && list[i][0].GetType().Name != "ChickenHouse")
         {
-            var list = farm.CreateFacilitiesList();
-            for (int i = 0; i < list.Count; i++)
-            {
-                Console.WriteLine(list[i][0].GetType().Name);
-                if (list[i][0].GetType().Name != "GrazingField" && list[i][0].GetType().Name != "ChickenHouse")
-                {
-                    list.RemoveAt(i);
-                    i--;
-                    Console.WriteLine(i);
-                }
-
-                //return animal.Butcher();
-
-            }
-
-            for (int i = 0; i < list.Count; i++)
-            {
-                Console.WriteLine($"{i}  {list[i][0].GetType().Name}");
-
-            }
-            Console.ReadLine();
+          list.RemoveAt(i);
+          i--;
         }
+      }
+
+      for (int i = 0; i < list.Count; i++)
+      {
+        Console.WriteLine($"{i}  {list[i][0].GetType().Name}");
+
+      }
+      Console.ReadLine();
     }
+  }
 }
